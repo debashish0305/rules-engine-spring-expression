9a32349cf5d039d25d4f5760a79ec1da7622c543
debashishbarik <debashish.barik@hotmail.com>
2023-07-06 15:16:06 +0530

    rule s changes

--- src/main/java/com/db/sprng/expression/rules/engine/rules/RulesEngine.java
+++ src/main/java/com/db/sprng/expression/rules/engine/rules/RulesEngine.java
@@ -4,12 +4,14 @@ import com.db.sprng.expression.rules.engine.model.Data;
 import com.db.sprng.expression.rules.engine.model.Rule;
 import com.db.sprng.expression.rules.engine.model.Rules;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class RulesEngine {
     private final RulesHelper rulesHelper;
@@ -30,7 +32,10 @@ public class RulesEngine {
                 double discountAmt = data.getAmount() * discount;
                 data.setDiscountAmount(discountAmt);
                 data.setAmount(data.getAmount() - discountAmt);
-                data.setMessage("Final price of the vehicle in " + data.getCity() + " is " + data.getAmount() + ", with discount rate of " + data.getDiscountRate()+" %");
+                String message = "Final price of the vehicle in " + data.getCity() + " is " + data.getAmount() + ", with discount rate of " + data.getDiscountRate() + " %";
+                log.info("Spel Rules Engine evaluted..");
+                log.info(message);
+                data.setMessage(message);
                 return data;
             }
         }
